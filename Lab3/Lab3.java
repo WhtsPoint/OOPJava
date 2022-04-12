d3b6ee78fe6868bb513e558d0af1c13d87f598d1
WhtsPoint <whtspoint@gmail.com>
2022-04-12 09:05:13 +0300

    Fix4

--- Lab3/Lab3.java
+++ Lab3/Lab3.java
@@ -1,28 +1,35 @@
 package Lab3;
 
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lab3 {
     public static void main(String[] args) {
 
-
         /*
             C3 = 20 % 3 = 2 (String)
-            C17 = 20 % 17 = 3 (Знайти таке слово в першому реченні заданого тексту, якого не має в жодному з наступних.)
+            C17 = 20 % 17 = 3 (В усіх питальних реченнях заданого тексту знайти та надрукувати без повторень слова заданої довжини.)
          */
 
         Scanner scanner = new Scanner(System.in);
 
-        String text = "Я бы бы бы хотел, чтобы меня называли все талант. Но но уже есть один талант под псевдонимом skorpion!";
+        String text = "Я бы бы бы хотел, чтобы меня называли все талант! Но но уже есть один талант под (псевдонимом)2 skorpion? А. було?";
 
         System.out.println("Введіть довжину рядка:");
 
         final int wordLength = scanner.nextInt();
 
+        Pattern pattern = Pattern.compile("[^.!?]+(?=\\?)");
+        Matcher matches = pattern.matcher(text);
 
-        String clearedText = text.replaceAll("\\?|\\!|\\.|\\,|\\-|\\:", "\s").replaceAll("\\s+", "\s");
+        String questionSentences = "";
+        while(matches.find())
+            questionSentences += matches.group();
+
+
+        String clearedText = questionSentences.replaceAll("[^а-яА-Яa-zA-ZЇїкєЕi]+", "\s").replaceAll("\\s+", "\s").trim();
 
         String[] wordList = clearedText.split("\s");
 
